eb2f91f3ac12f388db10c923fd94a97b7c99e0bb
_.danloncer._ <ermolaev.daniil2004@mail.ru>
2021-11-10 18:55:10 +0300

    Initial commit

---
+++ src/main/java/pro/sky/EmployeeWorkListWithCollections/EmployeeWorkListWithCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeWorkListWithCollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWorkListWithCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWorkListWithCollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/EmployeeWorkListWithCollections/EmployeeWorkListWithCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeWorkListWithCollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWorkListWithCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
